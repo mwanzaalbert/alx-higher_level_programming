bbc53e169b7a4dd49cfc1fbad778dfa8b14b0f7a
mwanzaalbert <user.email>
2022-10-29 02:00:01 -0700

    11-best_score.sql amended

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- select records are greater than or equal to a given condition
-SELECT score, name FROM second_table WHERE score >=10;
+SELECT score, name FROM second_table WHERE score >=10 ORDER BY score DESC;
