397a745154fa1b7452f6d2a6ef7b3f53ab36940d
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2023-06-13 15:38:06 +0300

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,11 +1,6 @@
-code = '''
 class MyInt(int):
     def __eq__(self, other):
         return super().__ne__(other)
 
     def __ne__(self, other):
         return super().__eq__(other)
-'''
-
-with open('100-my_int.py', 'w') as file:
-    file.write(code)
