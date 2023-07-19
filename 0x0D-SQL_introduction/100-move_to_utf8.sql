4ba18ece291ac52aad610d2ad9e3805436e51bad
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2023-07-19 07:32:17 +0300

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,9 @@
 -- alter character set and collocation for database, table and column
-ALTER SCHEMA hbtn_0c_0  CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+-- Step 1: Convert the database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table
-	CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+-- Step 2: Convert the table to UTF8
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table
-	MODIFY name CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+-- Step 3: Convert the field to UTF8
+ALTER TABLE hbtn_0c_0.first_table CHANGE name name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
