653c77830acc03adc21d71d82fef202853bcc135
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2023-07-20 16:57:02 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,14 @@
 -- a script that creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES 
-ON *.*
-TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+-- Check if the user 'user_0d_1' already exists
+SELECT 1 INTO @user_exists
+FROM mysql.user
+WHERE user = 'user_0d_1' AND host = 'localhost';
+
+-- If the user does not exist, create the user and grant all privileges
+SET @sql = IF(@user_exists IS NULL, 'CREATE USER \'user_0d_1\'@\'localhost\' IDENTIFIED BY \'user_0d_1_pwd\';', '');
+SET @sql = CONCAT(@sql, 'GRANT ALL PRIVILEGES ON *.* TO \'user_0d_1\'@\'localhost\'; FLUSH PRIVILEGES;');
+
+-- Execute the SQL statements
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
