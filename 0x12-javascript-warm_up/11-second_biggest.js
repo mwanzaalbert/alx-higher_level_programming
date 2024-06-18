ee160df06b4378f54c8542d086baea50e336400b
root <71031723+mwanzaalbert@users.noreply.github.com>
2024-06-18 09:48:49 -0700

    11-second_biggest.js added

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const argsData = process.argv.slice(2).map(Number);
+
+const secondLargest = getSecondLargest(argsData);
+
+function getSecondLargest (argList) {
+  if (argList.length < 2) {
+    return 1;
+  } else {
+    const sortedData = [...(argList)].sort();
+    return sortedData[sortedData.length - 2];
+  }
+}
+
+console.log(secondLargest);
