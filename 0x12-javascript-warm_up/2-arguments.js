7e53ee44a733b7379b150ae38c7501387c99bea4
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-06-18 12:07:11 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const arg_passed = process.argv.length;
+const argsPassed = process.argv.length;
 
-if (arg_passed > 2) {
-	console.log('Argument' + (arg_passed > 3 ? 's' : '') + ' found');
+if (argsPassed > 2) {
+	console.log('Argument' + (argsPassed > 3 ? 's' : '') + ' found');
 } else {
 	console.log('No argument');
 }
