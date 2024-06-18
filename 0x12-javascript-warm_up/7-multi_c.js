ab0d17c7a2c43f8ff27fe034b118be59056f76df
root <71031723+mwanzaalbert@users.noreply.github.com>
2024-06-18 03:58:05 -0700

    +x

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const numPrints = Number.parseInt(process.argv[2]);
 
-if (numPrints === NaN){
-	console.log("Missing number of occurrence");
-}else if (numPrints >=1 ){
-	for (let counter=0; counter < numPrints; counter++){
-		console.log('C is fun');
-	}
+if (Number.isNaN(numPrints)) {
+  console.log('Missing number of occurrence');
+} else if (numPrints >= 1) {
+  for (let counter = 0; counter < numPrints; counter++) {
+    console.log('C is fun');
+  }
 }
