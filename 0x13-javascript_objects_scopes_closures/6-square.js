db4750a38e7325841b98e7c3591e089d29ad9ce5
root <71031723+mwanzaalbert@users.noreply.github.com>
2024-06-21 03:32:06 -0700

    semistandard compliance

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,18 +9,18 @@ class Square extends SquareParent {
    * Prints this Square with the given character, otherwise 'X'.
    * @param {String} c The character to print this Square with.
    */
-  charPrint(toPrint=''){
-		if (toPrint){
-			for(let counter = 0, row = this.height; counter < row; counter++){
-				for(let count = 0, col = this.width; count < col; count++){
-					process.stdout.write(toPrint);
-				}
-				console.log();
-			}
-		}else{
-			this.print();
-		}
-	}
+  charPrint (toPrint = '') {
+    if (toPrint) {
+      for (let counter = 0, row = this.height; counter < row; counter++) {
+        for (let count = 0, col = this.width; count < col; count++) {
+          process.stdout.write(toPrint);
+        }
+        console.log();
+      }
+    } else {
+      this.print();
+    }
+  }
 }
 
 module.exports = Square;
