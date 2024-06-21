b52709741aa509695a6fdc5b501d556b1cb548a2
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-06-21 11:39:59 +0300

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -18,20 +18,21 @@ class Rectangle {
   /**
    * Prints this Rectangle with the character 'X'.
    */
-  print () {
-    const row = new Array(this.width).fill('X', 0, this.width);
-    const rows = new Array(this.height).fill(row.join(''), 0, this.height);
-    console.log(rows.join('\n'));
-  }
+  print(){
+		for(let counter = 0, row = this.height; counter < row; counter++){
+			for(let count = 0, col = this.width; count < col; count++){
+				process.stdout.write("X");
+			}
+			console.log();
+		}
+	}
 
   /**
    * Swaps the width and height of this Rectangle.
    */
-  rotate () {
-    const tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
+  rotate(){
+		[this.width, this.height] = [this.height, this.width];
+	}
 
   /**
    * Doubles the dimensions of this Rectangle.
