cac1032e614bed93ab82f6a6e858abd362046123
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-06-21 13:59:23 +0300

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 
-const newList = list.map((val, idx) => val * idx);
+const newList = list.map((val, index) => val * index);
 console.log(list);
 console.log(newList);
