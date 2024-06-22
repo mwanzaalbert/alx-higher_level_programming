357a85d09cc4542b7ed25647d3a1a4e4922a3320
mwanzaalbert <user.email>
2024-06-22 19:50:15 +0300

    query amended again

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -60,11 +60,8 @@ def fetch_and_print_state(cursor, to_search):
                 query.
         to_search (str): The name of the state to search for.
     """
-    query = """
-    SELECT * FROM states
-    WHERE name BINARY LIKE {}
-    ORDER BY id ASC;
-    """.format(to_search)
+    query = "SELECT * FROM states WHERE name BINARY LIKE " +\
+        "{} ORDER BY id ASC".format(to_search)
 
     cursor.execute(query)
     query_rows = cursor.fetchall()
