cd2be94d761fa3984be6f2fb2562e5f722b16898
mwanzaalbert <user.email>
2024-06-24 11:13:41 +0300

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -60,7 +60,7 @@ def fetch_and_print_state(cursor, to_search):
                 query.
         to_search (str): The name of the state to search for.
     """
-    query = "SELECT * FROM states WHERE name BINARY LIKE %s ORDER BY id ASC"
+    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
     cursor.execute(query, (to_search,))
     query_rows = cursor.fetchall()
     for row in query_rows:
