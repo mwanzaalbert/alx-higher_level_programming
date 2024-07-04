569f57156d0ade01f5b0e4833519c6b6b16c0625
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-07-04 19:14:24 +0300

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a GET request, and displays the body of the response for status code 200
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -s -o /tmp/body_response -w "%{http_code}" "$1"
+[ "$(tail -n 1 /tmp/body_response)" = "200" ] && cat /tmp/body_response | head -n -1
