99757c6c36d4ed08044479690d7e80300a1a53a2
mwanzaalbert <user.email>
2024-07-05 22:15:32 +0300

    updated

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+"""Send a request to a URL and prints its response or error code."""
 import urllib.request
 import urllib.error
 import sys
 
-url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+def fetch_url(url):
+    """Send a request to a URL and prints its response or error code."""
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+
+if __name__ == "__main__":
+    fetch_url(sys.argv[1])
