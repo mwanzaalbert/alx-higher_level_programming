424d690c114720959f24ed36a59b87fefbbb1d07
mwanzaalbert <user.email>
2024-07-05 22:21:37 +0300

    updated

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""Fetch a header of a response from a URL."""
 import requests
 import sys
 
-url = sys.argv[1]
 
-response = requests.get(url)
-x_request_id = response.headers.get('X-Request-Id')
+def fetch_x_request_id(url):
+    """Fetch a header of a response from a URL."""
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+    if x_request_id:
+        print(x_request_id)
+    else:
+        print("X-Request-Id header not found")
 
-print(x_request_id)
+
+if __name__ == "__main__":
+    fetch_x_request_id(sys.argv[1])
