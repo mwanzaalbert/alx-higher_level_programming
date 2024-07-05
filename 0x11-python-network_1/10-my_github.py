3d02fc77b2c8c2850e03a2327748795d284c931b
mwanzaalbert <user.email>
2024-07-05 22:39:34 +0300

    done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""Print the GitHub id of a user."""
 import requests
 import sys
 
-username = sys.argv[1]
-token = sys.argv[2]
 
-url = "https://api.github.com/user"
+def get_github_id(username, token):
+    """Print the GitHub id of a user."""
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, token))
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data.get('id'))
+    else:
+        print("None")
 
-response = requests.get(url, auth=(username, token))
-json_response = response.json()
 
-print(json_response.get('id'))
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    get_github_id(username, token)
