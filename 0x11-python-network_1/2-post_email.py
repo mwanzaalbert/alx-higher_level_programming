21e982311912fd46783000376288ea03d47c7cf2
mwanzaalbert <user.email>
2024-07-05 22:07:42 +0300

    updated again

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,12 +4,14 @@ import urllib.request
 import urllib.parse
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-with urllib.request.urlopen(url, data) as response:
-    body = response.read()
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-    print(body.decode('utf-8'))
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read()
+
+        print(f"Your emails is {body.decode('utf-8')}")
