ba62953cd931e3cc422e86eb883a20820e4133cd
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-07-05 21:03:21 +0300

    Updated 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Fetches a header of a response from a URL."""
+import urllib.request
 import sys
-from urllib import request
 
-
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-        with request.urlopen(url) as response:
-            print(response.headers['X-Request-Id'])
+if __name__ == "__main__":
+    url = sys.argv[1]
+    
+    # Making the request and handling the response
+    req = urllib.request.Request(url)
+    
+    with urllib.request.urlopen(req) as response:
+        # Fetching the value of X-Request-Id header
+        x_request_id = response.getheader('X-Request-Id')
+        
+        if x_request_id:
+            print(x_request_id)
