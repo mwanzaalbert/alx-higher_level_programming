9ba353855048786b5bf40dc3381ed8d207df40ef
mwanzaalbert <user.email>
2024-07-05 22:32:08 +0300

    done

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""Send a request to a URL and prints its response or error code."""
 import requests
 import sys
 
-url = sys.argv[1]
 
-response = requests.get(url)
+def fetch_url(url):
+    """Send a request to a URL and prints its response or error code."""
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
 
-if response.status_code >= 400:
-    print("Error code: {}".format(response.status_code))
-else:
-    print(response.text)
+
+if __name__ == "__main__":
+    fetch_url(sys.argv[1])
