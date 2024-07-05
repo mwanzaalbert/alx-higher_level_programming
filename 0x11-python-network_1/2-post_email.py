09a75fa75f25d79d149d5a09e7a1998bfacb1ef3
mwanzaalbert <user.email>
2024-07-05 22:50:58 +0300

    updated

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,13 +5,17 @@ import urllib.parse
 import sys
 
 
-if __name__ == '__main__':
+def post_email(url, email):
+    """Send a form data to a URL."""
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data)
+
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
+
+
+if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-
-    with urllib.request.urlopen(url, data) as response:
-        body = response.read()
-        print(response.read().decode('utf-8'))
-        # print(f"Your email is: {body.decode('utf-8')}")
+    post_email(url, email)
