673842573eba03feb73a6705513b62f9d20a9d83
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-07-30 18:55:33 +0300

    Updated 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,20 +4,22 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18;
 
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (body){
-    const data = JSON.parse(body);
-    const films = data.results;
-    let count = 0;
-
-    films.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        count++;
-      }
-    });
-
-    console.log(count);
-  }
-});
+if (process.argv.length > 2) {
+  request.get(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else{
+      const data = JSON.parse(body);
+      const films = data.results;
+      let count = 0;
+  
+      films.forEach(film => {
+        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+          count++;
+        }
+      });
+  
+      console.log(count);
+    }
+  });
+}
