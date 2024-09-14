f15a27f962ad3e6a0891161563c103f3a7657ef0
mwanzaalbert <user.email>
2024-09-14 00:39:44 -0700

    updated 100-print_tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,6 @@
-
+#!/usr/bin/python3
+for i, letter in enumerate(list(chr(index) for index in range(ord("A"), ord('Z') + 1))[::-1]):
+    if i % 2 == 0:
+        print("{:s}".format(letter.lower()), end='')
+    else:
+        print("{:s}".format(letter), end='')
