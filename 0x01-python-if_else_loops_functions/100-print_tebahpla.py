5f8bf19442089936a8b0ab8037050562c9589212
mwanzaalbert <user.email>
2024-09-14 00:46:57 -0700

    pycodestyle formatting

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i, letter in enumerate(list(chr(index) for index in range(ord("A"), ord('Z') + 1))[::-1]):
+for i, letter in enumerate(list(
+    chr(index) for index in range(ord("A"),
+                                  ord('Z') + 1))[::-1]):
     if i % 2 == 0:
         print("{:s}".format(letter.lower()), end='')
     else:
