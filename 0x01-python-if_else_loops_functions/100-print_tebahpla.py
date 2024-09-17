dd3084b0f70875e0d1eb5f42d73c817c4ca3b90a
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-09-17 15:47:29 +0300

    renamed loop variable

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for x in range(ord('Z'), ord('A') - 1, -1):
-    (print(chr(x).lower(), end="") if x % 2 == 0
-     else print(chr(x), end=""))
+for index in range(ord('Z'), ord('A') - 1, -1):
+    (print(chr(index).lower(), end="") if index % 2 == 0
+     else print(chr(index), end=""))
 
 # for i, letter in enumerate(list(
 #     chr(index) for index in range(ord("A"),
