53eb2525375d82bfa2e3d07810fba8740373b10a
mwanzaalbert <user.email>
2024-10-21 18:42:11 +0300

    refactored code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 
+import hidden_4
+
+
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
-    import hidden_4
-
+    # Get all names defined by the hidden_4 module
     names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+
+    # Filter names that do not start with '__'
+    filtered_names = [name for name in names if not name.startswith("__")]
+
+    # Sort names in alphabetical order
+    # filtered_names.sort()
+
+    # Print each name on a new line
+    for name in filtered_names:
+        print(name)
