5fed94123208b4addc8f51ce7c710a3c2bd1cdce
mwanzaalbert <user.email>
2024-11-01 15:56:04 +0300

    removed doctest and typed annotation

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,45 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from typing import List
 
 
-def print_list_integer(my_list: List[int] = []) -> None:
-    """
-    Print each integer in a list on a new line.
-
-    Args:
-        my_list (List[int]): List of integers to print.
-
-    Raises:
-        ValueError: If an element in the list is not an integer.
-
-    Examples:
-        >>> print_list_integer([1, 2, 3, 4, 5])
-        1
-        2
-        3
-        4
-        5
-
-        >>> print_list_integer([])
-
-        >>> print_list_integer([1])
-        1
-
-        >>> print_list_integer([1, 2, 3])
-        1
-        2
-        3
-
-        >>> print_list_integer([1, 2, "H", 9])
-        Traceback (most recent call last):
-            ...
-        ValueError: Unknown format code 'd' for object of type 'str'
-    """
+def print_list_integer(my_list=[]):
     for i in my_list:
         print('{:d}'.format(i))
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod(verbose=True)
