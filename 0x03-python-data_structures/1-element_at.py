275f1da1a7db5a0cdda386912208c58ba6191152
mwanzaalbert <user.email>
2024-11-01 16:14:13 +0300

    embeded try/except in function

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return ("None")
+    try:
+        assert idx in range(0, len(my_list))
+    except AssertionError:
+        return None
     else:
         return my_list[idx]
