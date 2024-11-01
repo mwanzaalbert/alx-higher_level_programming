4f80f1f71a1046574bc12462360d00fd7f984359
mwanzaalbert <user.email>
2024-11-01 20:42:02 +0300

    removed redundant copy in membership test

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     copy = my_list.copy()
     if idx not in range(0, len(my_list)):
-        return my_list.copy()
+        return copy
 
     copy[idx] = element
     return copy
