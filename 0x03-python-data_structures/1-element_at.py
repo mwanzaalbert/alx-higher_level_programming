2d7b9c4c861ec87d6d8ef09fbc0b4958c6faf52d
mwanzaalbert <user.email>
2024-11-01 16:51:33 +0300

    removed try/except implemetation

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    try:
-        assert idx in range(0, len(my_list))
-    except AssertionError:
-        return None
-    else:
-        return my_list[idx]
+    return my_list[idx] if idx in range(0, len(my_list)) else None
