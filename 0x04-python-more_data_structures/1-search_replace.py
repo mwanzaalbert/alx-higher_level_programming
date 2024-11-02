e6d86de559a43f464f513a9c8efb2add2792de7e
mwanzaalbert <user.email>
2024-11-02 23:05:46 +0300

    Code refactoring; removed helper function; used lambda fuction in implementtion

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def find_search(element):
-        return element if element != search else replace
-    return list(map(find_search, my_list))
+    return list(map(lambda x: replace if x == search else x, my_list))
