e1ce037e21c306a293429782b0e51113e1e39bd5
mwanzaalbert <user.email>
2024-11-13 11:31:51 +0300

    code refactoring

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
-        return 0
+ROMAN_NUMERALS = {'M': 1000, 'D': 500, 'C': 100,
+                  'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+
+def roman_to_int(roman_literal):
+    if not roman_literal or not isinstance(roman_literal, str):
+        return 0  # or raise ValueError
+
     total = 0
-    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for roman in reversed(roman_string):
-        arabic = digits[roman]
-        total += arabic if total < arabic * 5 else -arabic
+    length = len(roman_literal)
+
+    for index, letter in enumerate(roman_literal):
+        # Get the current numeral's value
+        current_value = ROMAN_NUMERALS[letter]
+
+        # Check if the current numeral should be subtracted (e.g., IV, IX)
+        if index + 1 < length and (
+                current_value < ROMAN_NUMERALS[roman_literal[index + 1]]):
+            total -= current_value
+        else:
+            total += current_value
+
     return total
+
+
+if __name__ == "__main__":
+    test_cases = ["X", "VII", "IX", "LXXXVII", "DCCVII", "CDVII", 'CMLX']
+
+    for roman_number in test_cases:
+        print(f"{roman_number} = {roman_to_int(roman_number)}")
