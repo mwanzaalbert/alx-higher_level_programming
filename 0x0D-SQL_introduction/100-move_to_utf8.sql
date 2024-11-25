486cd51ba8f2485fdc491e7c799c781a6ba54f61
mwanzaalbert <user.email>
2024-11-25 13:48:07 +0300

    code revision

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,19 +1,7 @@
 -- Set the default character set and collation for the database
-ALTER DATABASE hbtn_0c_0 
-CHARACTER SET 'utf8mb4' 
-COLLATE 'utf8mb4_unicode_ci';
-
--- Use the correct database
+ALTER DATABASE hbtn_0c_0
+    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- -- Convert the table character set and collation
 USE hbtn_0c_0;
-
--- Convert the table character set and collation
-ALTER TABLE first_table 
-CONVERT TO CHARACTER SET 'utf8mb4' 
-COLLATE 'utf8mb4_unicode_ci';
-
--- Explicitly modify the `name` column's collation
-ALTER TABLE first_table 
-MODIFY COLUMN name VARCHAR(256) 
-CHARACTER SET 'utf8mb4' 
-COLLATE 'utf8mb4_unicode_ci' 
-DEFAULT NULL;
+ALTER TABLE first_table
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
