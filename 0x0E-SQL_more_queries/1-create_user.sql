46456938adda35f731cd48fe0cb59e7d2ac8b65c
mwanzaalbert <user.email>
2024-11-26 11:28:53 +0300

    git push

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 --  creates the MySQL server user user_0d_1
 CREATE OR REPLACE USER user_0d_1 IDENTIFIED BY PASSWORD 'user_0d_1_pwd' ;
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
