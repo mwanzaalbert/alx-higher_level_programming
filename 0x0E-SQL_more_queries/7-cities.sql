0bb9c1c041a196d2eec805b067d7a134558f2bda
mwanzaalbert <user.email>
2024-11-26 13:57:59 +0300

    code refactor

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,11 @@
---  a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-  `id` INT UNIQUE NOT NULL AUTO_INCREMENT,
-  `state_id` INT NOT NULL,
-  `name` VARCHAR(256) NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`));
+-- create the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+id INT NOT NULL AUTO_INCREMENT UNIQUE,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+CONSTRAINT FK_State_id
+FOREIGN KEY (state_id)
+REFERENCES states(id));
