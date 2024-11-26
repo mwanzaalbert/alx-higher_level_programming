c8311bbf179df99c5d173d02e8663f18f7cccadd
mwanzaalbert <user.email>
2024-11-26 22:59:53 +0300

    update

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -7,5 +7,6 @@
 SELECT G.name, COUNT(S.genre_id) AS  number_of_shows
 FROM tv_genres G
 RIGHT JOIN tv_show_genres S ON S.genre_id=G.id
+WHERE S.genre_id IS NOT NULL
 GROUP BY G.name
 ORDER BY number_of_shows DESC;
