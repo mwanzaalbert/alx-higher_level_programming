2ddc5cf09c5578b707f154125af61449a79ebce5
mwanzaalbert <user.email>
2024-11-26 12:04:55 +0300

    code refactor

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,4 @@
--- Check if the user already exists
-SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_1') INTO @user_exists;
-
--- If the user does not exist, create the user and grant privileges
-IF @user_exists = 0 THEN
-    CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-    FLUSH PRIVILEGES;
-END IF;
+-- create user with all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
