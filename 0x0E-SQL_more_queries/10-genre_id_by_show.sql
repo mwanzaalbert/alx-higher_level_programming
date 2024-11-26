e3d1ee20a78d8e9b96a483080a070080915b8c59
mwanzaalbert <user.email>
2024-11-26 22:24:53 +0300

    code refacor

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
--- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_shows.title, tv_show_genres.genre_id
-  FROM tv_show_genres
-  INNER JOIN tv_shows
-  ON tv_shows.id = tv_show_genres.show_id
-  ORDER BY tv_shows.title, tv_show_genres.genre_id;
+-- list Genre ID by show
+SELECT S.title, G.genre_id
+FROM tv_shows S
+RIGHT JOIN tv_show_genres G ON G.show_id=S.id
+ORDER BY S.title, G.genre_id ASC;
