a9e9d94e522bbb94fa03da297745deb9a0dd24c1
mwanzaalbert <user.email>
2024-12-15 10:36:43 +0300

    code refactoring

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,23 +1,64 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-"""A module that defines a square """
+"""A module that defines a square."""
+
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-15"
+__version__ = "1.1"
 
 
 class Square:
-    """A class that represents a square"""
+    """A class that represents a square."""
 
     def __init__(self, size=0):
-        """Initializing this square class
-        Args:
+        """Initialize a Square instance.
+
+        Args_:
             size: represnets the size of the square defined
-        Raises:
+        Raises_:
             TypeError: if size is not integer
             ValueError: if size is less than zero
         """
-
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
 
         self.__size = size
+
+
+# Test Code
+if __name__ == '__main__':
+    my_square_1 = Square(3)
+    print(type(my_square_1))
+    print(my_square_1.__dict__)
+
+    my_square_2 = Square()
+    print(type(my_square_2))
+    print(my_square_2.__dict__)
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_3 = Square("3")
+        print(type(my_square_3))
+        print(my_square_3.__dict__)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_4 = Square(-89)
+        print(type(my_square_4))
+        print(my_square_4.__dict__)
+    except Exception as e:
+        print(e)
