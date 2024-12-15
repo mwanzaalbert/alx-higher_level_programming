bd633bd54c62833b6b5be6d5da11b87fc4f98772
mwanzaalbert <user.email>
2024-12-15 09:23:20 +0300

    code refactoring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-"""Define a class Square."""
+"""Define a class Square with a constructor method."""
+
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-15"
+__version__ = "1.1"
 
 
 class Square:
-    """Represent a square."""
+    """Represents a square."""
 
     def __init__(self, size):
         """Initialize a new Square.
@@ -12,3 +18,19 @@ class Square:
             size (int): The size of the new square.
         """
         self.__size = size
+
+
+if __name__ == '__main__':
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
