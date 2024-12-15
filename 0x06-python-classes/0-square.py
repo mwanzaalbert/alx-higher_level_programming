76ed6b105016e44c32240cec4b0c2d05a5382156
mwanzaalbert <user.email>
2024-12-15 09:13:31 +0300

    code refactoring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-"""Defines a square """
+"""Define an empty Square class."""
 
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-15"
+__version__ = "1.1"
 
 class Square:
-    """Does nothing"""
+    """Does nothing."""
     pass
+
+
+if __name__ == '__main__':
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
