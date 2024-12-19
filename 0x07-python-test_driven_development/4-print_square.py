b034ff296d76565451f9550be1b538487180fe84
mwanzaalbert <user.email>
2024-12-19 16:54:20 +0300

    Code refactoring

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,23 +1,53 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
-Function that prints a square
+Module: print_square.
+
+This module defines a single function, `print_square`, which prints a square
+made of the `#` character. The size of the square is determined by the input
+parameter `size`. The function validates the input to ensure it is a
+non-negative integer.
+
+Functions:
+----------
+- print_square(size): Prints a square of a specified size using the `#`
+  character.
+
+Exceptions:
+-----------
+- Raises `TypeError` if `size` is not an integer.
+- Raises `ValueError` if `size` is a negative integer.
+
 """
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-19"
+__version__ = "1.1"
 
 
 def print_square(size):
     """
-    Function that prints a square
-    with the character #
+    Print a square of `#` characters of the given size.
 
-    size: is the size length of th esquare
+    Parameters_:
+    -----------
+    size : int
+        The size of the square (number of rows and columns).
+        Must be a non-negative integer.
+
+    Raises_:
+    -------
+    TypeError
+        If `size` is not an integer.
+    ValueError
+        If `size` is a negative integer.
+
     """
-
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
 
-    for i in range(size):
-        print("{}".format("#" * size))
+    for _ in range(size):
+        print("#" * size)
