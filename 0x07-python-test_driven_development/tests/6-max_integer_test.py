f1ef4edf0b2eb5f993b3e6b2dc705452403282bb
mwanzaalbert <user.email>
2024-12-19 20:54:26 +0300

    Code Refactoring

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,61 @@
 #!/usr/bin/python3
-"""Unittest for max_integer
+# -*- coding: utf-8 -*-
 """
-import unittest
+Unittest for max_integer function.
 
+This module contains a collection of test cases to validate the correctness
+of the `max_integer` function. The function is designed to find and return
+the maximum integer in a list of integers. If the list is empty, the function
+returns None.
+
+The test cases cover the following scenarios:
+- The maximum integer is at the end of the list.
+- The maximum integer is at the beginning of the list.
+- The maximum integer is in the middle of the list.
+- The list contains one negative number.
+- The list contains only negative numbers.
+- The list has a single element.
+- The list is empty.
+
+Each test case ensures the function behaves as expected under the specified
+conditions.
+"""
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-19"
+__version__ = "1.1"
+
+import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """unittests for the function def max_integer(list=[])"""
+    """Unittest class for max_integer function."""
 
-    def test_max_integer(self):
-        self.assertEqual(max_integer([1, 2, 3]), 3)
-        self.assertEqual(max_integer([7, -2, 5]), 7)
-        self.assertEqual(max_integer([7, 10, 5]), 10)
-        self.assertEqual(max_integer([-1, -5, -7]), -1)
+    def test_max_at_end(self):
+        """Test for the maximum integer at the end of the list."""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_at_beginning(self):
+        """Test for the maximum integer at the beginning of the list."""
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
+    def test_max_in_middle(self):
+        """Test for the maximum integer in the middle of the list."""
+        self.assertEqual(max_integer([1, 4, 2, 3]), 4)
+
+    def test_one_negative_number(self):
+        """Test for a list with one negative number."""
+        self.assertEqual(max_integer([1, -2, 3, 4]), 4)
+
+    def test_all_negative_numbers(self):
+        """Test for a list with only negative numbers."""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_single_element(self):
+        """Test for a list with a single element."""
         self.assertEqual(max_integer([7]), 7)
-        self.assertEqual(max_integer([]), None)
+
+    def test_empty_list(self):
+        """Test for an empty list."""
+        self.assertIsNone(max_integer([]))
