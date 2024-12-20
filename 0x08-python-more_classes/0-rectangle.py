15e12e1b680e0d744ce2ff5dcf97c92f4aad044c
mwanzaalbert <user.email>
2024-12-20 21:41:08 +0300

    code refactoring

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Define an empty Rectangle class."""
 
-"""Defines a Rectangle class."""
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-20"
+__version__ = "1.1"
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Do nothing."""
+
     pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
