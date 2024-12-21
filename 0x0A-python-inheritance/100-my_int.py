1fa3fa3dfb29081e14371ce0dfcd6dda954cfdde
mwanzaalbert <user.email>
2024-12-22 02:17:41 +0300

    code refactoring

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Integer subclassing module.
+
+This module defines the MyInt class, a rebellious integer class.
+
+MyInt inverts the behavior of the equality (`==`) and inequality (`!=`)
+operators.
+"""
+
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-22"
+__version__ = "1.1"
+
+
 class MyInt(int):
+    """A rebel integer class where `==` and `!=` are inverted."""
+
     def __eq__(self, other):
+        """Override `==` to invert its behavior."""
         return super().__ne__(other)
 
     def __ne__(self, other):
+        """Override `!=` to invert its behavior."""
         return super().__eq__(other)
+
+
+if __name__ == "__main__":
+    my_i = MyInt(3)
+    print(my_i)          # Output: 3
+    print(my_i == 3)     # Output: False
+    print(my_i != 3)     # Output: True
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,56 @@
 #!/usr/bin/python3
-def add_attribute(obj, attribute_name, value):
-    if hasattr(obj, attribute_name):
+# -*- coding: utf-8 -*-
+"""The module defines a function to add a new attribute to an object."""
+
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-22"
+__version__ = "1.1"
+
+
+def add_attribute(obj, name, value):
+    """
+    Add a new attribute to an object if possible.
+
+    Args_:
+        obj (object): The object to which the attribute will be added.
+        name (str): The name of the attribute.
+        value (any): The value of the attribute.
+
+    Raises_:
+        TypeError: If the object cannot have new attributes.
+    """
+    if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
-    else:
-        setattr(obj, attribute_name, value)
+
+    setattr(obj, name, value)
+
+
+if __name__ == "__main__":
+    class MyClass():
+        """Empty class."""
+
+        pass
+
+    mc = MyClass()
+    add_attribute(mc, "name", "John")
+    print(mc.name)  # Output: John
+
+    try:
+        a = "My String"
+        add_attribute(a, "name", "Bob")
+        print(a.name)
+    except Exception as e:
+        # Output: [TypeError] can't add new attribute
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    add_attribute(mc, "age", "30")
+    print(mc.age)
+
+    try:
+        a = "My String"
+        add_attribute(mc, "age", "40")
+        print(a.name)
+    except Exception as e:
+        # Output: [TypeError] can't add new attribute
+        print("[{}] {}".format(e.__class__.__name__, e))
