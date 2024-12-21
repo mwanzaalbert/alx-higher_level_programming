e61dd418b872bef3dd393453648a3d37b8188f2e
mwanzaalbert <user.email>
2024-12-21 14:26:37 +0300

    code refactoring

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
-"""Defines a locked class."""
+# -*- coding: utf-8 -*-
+"""
+LockedClass module.
+
+This module defines the LockedClass, a class that restricts the user
+from dynamically creating new instance attributes except for
+`first_name`.
+"""
+
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-21"
+__version__ = "1.1"
 
 
 class LockedClass:
     """
-    Prevent the user from instantiating new LockedClass attributes
-    for anything but attributes called 'first_name'.
+    Prevent dynamic creation of new instance attributes,
+    except for 'first_name'.
     """
 
-    __slots__ = ["first_name"]
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """Initialize a LockedClass instance."""
+        pass
+
+
+if __name__ == "__main__":
+    lc = LockedClass()
+    lc.first_name = "John"
+    try:
+        lc.last_name = "Snow"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
