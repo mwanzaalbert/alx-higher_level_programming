4b3114d87f3416f54d08b8f66ef4595d44ba5db1
mwanzaalbert <user.email>
2024-12-21 18:26:49 +0300

    code refactoring

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-
-"""Defines a class and inherited class-checking function."""
+# -*- coding: utf-8 -*-
+"""The module defines a class and inherited class-checking function."""
 
 
 def is_kind_of_class(obj, a_class):
-    """Check if an object is an instance or inherited instance of a class.
-
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is an instance or inherited instance of a_class - True.
-        Otherwise - False.
     """
-    if isinstance(obj, a_class):
-        return True
-    return False
+    Determine if an object is an instance or an inherited instance of a class.
+
+    Args_:
+        obj (any): The object to evaluate.
+        a_class (type): The class to check against.
+
+    Returns_:
+        bool: True if the object is an instance or an inherited instance
+              of the specified class, False otherwise.
+    """
+    return isinstance(obj, a_class)
+
+
+if __name__ == "__main__":
+    a = 1
+    if is_kind_of_class(a, int):
+        print("{} is an instance of the class {}".format(a, int.__name__))
+
+    if is_kind_of_class(a, float):
+        print("{} is an instance of the class {}".format(a, float.__name__))
+
+    if is_kind_of_class(a, object):
+        print("{} is an instance of the class {}".format(a, object.__name__))
