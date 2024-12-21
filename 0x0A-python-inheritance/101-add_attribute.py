b97eda0344bf0bcd06a2a2e2f446b2688018c3df
mwanzaalbert <user.email>
2024-12-22 02:19:49 +0300

    try/exeption in test code commented out

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -36,21 +36,21 @@ if __name__ == "__main__":
     add_attribute(mc, "name", "John")
     print(mc.name)  # Output: John
 
-    try:
-        a = "My String"
-        add_attribute(a, "name", "Bob")
-        print(a.name)
-    except Exception as e:
-        # Output: [TypeError] can't add new attribute
-        print("[{}] {}".format(e.__class__.__name__, e))
+    # try:
+    #     a = "My String"
+    #     add_attribute(a, "name", "Bob")
+    #     print(a.name)
+    # except Exception as e:
+    #     # Output: [TypeError] can't add new attribute
+    #     print("[{}] {}".format(e.__class__.__name__, e))
 
-    add_attribute(mc, "age", "30")
-    print(mc.age)
+    # add_attribute(mc, "age", "30")
+    # print(mc.age)
 
-    try:
-        a = "My String"
-        add_attribute(mc, "age", "40")
-        print(a.name)
-    except Exception as e:
-        # Output: [TypeError] can't add new attribute
-        print("[{}] {}".format(e.__class__.__name__, e))
+    # try:
+    #     a = "My String"
+    #     add_attribute(mc, "age", "40")
+    #     print(a.name)
+    # except Exception as e:
+    #     # Output: [TypeError] can't add new attribute
+    #     print("[{}] {}".format(e.__class__.__name__, e))
