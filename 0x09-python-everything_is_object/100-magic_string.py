8f1b7cf553f3a7b5eb3c5276dd3e1bb64cf01f00
mwanzaalbert <user.email>
2024-12-21 12:57:38 +0300

    Code refactoring

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(i=[0]):
-    i[0] += 1
-    return str("BestSchool, " * (i[0] - 1)) + "BestSchool"
+def magic_string():
+    magic_string.count = getattr(magic_string, "count", 0) + 1
+    return ", ".join(["BestSchool"] * magic_string.count)
