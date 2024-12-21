5c876bb872efd022d9088bfb4851e76a72c6ef8e
mwanzaalbert <user.email>
2024-12-22 01:52:49 +0300

    code refactoring

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Square module.
 
-"""Defines a Rectangle subclass Square."""
+This module defines a Square class that inherits from Rectangle class defined
+in 9-rectangle.py.
+"""
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-22"
+__version__ = "1.1"
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """Represent a square using the Rectangle class."""
 
     def __init__(self, size):
-        """Initialize a new square.
+        """
+        Initialize a new Square instance.
 
-        Args:
-            size (int): The size of the new square.
+        Args_:
+            size (int): The size of one side of the square.
         """
         self.integer_validator("size", size)
+
         super().__init__(size, size)
+
         self.__size = size
+
+
+if __name__ == "__main__":
+    s = Square(13)
+
+    print(s)
+    print(s.area())
