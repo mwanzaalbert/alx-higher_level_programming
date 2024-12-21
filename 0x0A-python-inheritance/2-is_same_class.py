ef6ad6b5da1d2bc3fef570d8ac19d1e33b177a1f
mwanzaalbert <user.email>
2024-12-21 18:18:09 +0300

    code refactoring

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-"""Defines a class-checking function."""
+"""The module defines a class-checking function."""
 
 
 def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of a given class.
-
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is exactly an instance of a_class - True.
-        Otherwise - False.
     """
-    if type(obj) == a_class:
+    Determine if an object is exactly an instance of a specified class.
+
+    Args_:
+        obj (any): The object to evaluate.
+        a_class (type): The class to check against.
+
+    Returns_:
+        bool: True if the object is exactly an instance of the specified class,
+              False otherwise.
+    """
+    if type(obj) is a_class:
         return True
     return False
+
+
+if __name__ == "__main__":
+    a = 1
+    if is_same_class(a, int):
+        print("{} is an instance of the class {}".format(a, int.__name__))
+
+    a = 1.0
+
+    if is_same_class(a, float):
+        print("{} is an instance of the class {}".format(a, float.__name__))
+
+    a = object()
+
+    if is_same_class(a, object):
+        print("{} is an instance of the class {}".format(a, object.__name__))
