6e5894c3e8d3ab8d09e51166f3df948c602aa924
mwanzaalbert <user.email>
2024-12-21 19:01:51 +0300

    code refactoring

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,11 +1,39 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+BaseGeometry module.
 
-"""Defines a base geometry class BaseGeometry."""
+This module defines an abstract base class BaseGeometry. The class contains
+a public instance method `area()` that raises an exception with the message
+'area() is not implemented'. This serves as a placeholder to be overridden
+by subclasses that represent specific geometric shapes.
+
+An empty class BaseGeometry is also provided to serve as a foundation for
+other geometric shapes.
+"""
 
 
 class BaseGeometry:
-    """Represent base geometry."""
+    """Abstract class for geometric shapes."""
 
     def area(self):
-        """Not implemented."""
+        """
+        Compute the area of a shape.
+
+        Raises_:
+            Exception: Always raises an exception because this method
+            is not implemented in the base class.
+        """
         raise Exception("area() is not implemented")
+
+
+if __name__ == "__main__":
+    # Create an instance of BaseGeometry
+    bg = BaseGeometry()
+
+    try:
+        # Attempt to call the area method, which will raise an exception
+        print(bg.area())
+    except Exception as e:
+        # Print the exception details
+        print("[{}] {}".format(e.__class__.__name__, e))
