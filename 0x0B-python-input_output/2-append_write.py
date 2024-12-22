cc15b5638b4ca9a556f458e7a543cd704f1e5ff6
mwanzaalbert <user.email>
2024-12-22 09:33:50 +0300

    code refactoring

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
 
-"""Defines a file-appending function."""
+"""The module defines a file-appending function."""
+
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-22"
+__version__ = "1.1"
 
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file.
+    """
+    Append a string to the end of a UTF8 text file.
 
-    Args:
+    Args_:
         filename (str): The name of the file to append to.
         text (str): The string to append to the file.
-    Returns:
+    Returns_:
         The number of characters appended.
     """
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
+    with open(filename, "a", encoding="utf-8") as outfile:
+        return outfile.write(text)
+
+
+if __name__ == "__main__":
+    nb_characters = append_write("txt_files/file_append.txt",
+                                 "This School is so cool!\n")
+    print(nb_characters)
