0ec1c4e761043a927458a10c9ba5f26c7c190fbd
mwanzaalbert <user.email>
2024-12-22 15:37:30 +0300

    code refactoring

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""The module defines a class that models a student."""
 
-"""Defines a class Student."""
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-22"
+__version__ = "1.1"
 
 
 class Student:
-    """Represent a student."""
+    """Represents a student with basic information.
+
+    Attributes_:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """Initialize a new Student instance.
 
-        Args:
+        Args_:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
             age (int): The age of the student.
@@ -19,5 +30,22 @@ class Student:
         self.age = age
 
     def to_json(self):
-        """Get a dictionary representation of the Student."""
+        """
+        Get a dictionary representation of the Student instance.
+
+        Returns_:
+            dict: A dictionary containing the student's attributes.
+        """
         return self.__dict__
+
+
+if __name__ == "__main__":
+    students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
+
+    for student in students:
+        j_student = student.to_json()
+        print(type(j_student))
+        print(j_student['first_name'])
+        print(type(j_student['first_name']))
+        print(j_student['age'])
+        print(type(j_student['age']))
