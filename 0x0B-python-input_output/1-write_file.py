87d70400970680297062488797eaad9e76063559
mwanzaalbert <user.email>
2024-12-22 09:28:30 +0300

    code refactoring

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""The module defines a file-writing function."""
 
-"""Defines a file-writing function."""
+__author__ = "Albert Mwanza"
+__license__ = "MIT"
+__date__ = "2024-12-22"
+__version__ = "1.1"
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file.
+    """
+    Write a string to a UTF8 text file.
 
-    Args:
+    Args_:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
-    Returns:
+    Returns_:
         The number of characters written.
     """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+    with open(filename, "w", encoding="utf-8") as outfile:
+        return outfile.write(text)
+
+
+if __name__ == "__main__":
+    nb_characters = write_file("txt_files/my_first_file.txt",
+                               "This School is so cool!\n")
+    print(nb_characters)
