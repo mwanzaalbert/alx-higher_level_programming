96830f649ce89f1d80a9f3aaa810bd0c24f223cb
mwanzaalbert <user.email>
2025-02-20 08:07:47 +0300

    refactor: refactor testing unequal rows

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -25,15 +25,23 @@ def lazy_matrix_mul(m_a, m_b):
 
     Raises_:
         TypeError: If m_a or m_b is not a list of lists, or if the elements are
-                   not integers/floats, or if rows have inconsistent size.
+                    not integers/floats, or if rows have inconsistent size.
         ValueError: If matrices are empty or the matrices cannot be multiplied
-                   due to incompatible dimensions.
+                    due to incompatible dimensions.
     """
 
     def elements_are_not_int_or_float(matrix):
         """Evaluate all elements in a matrix if not an int or float."""
         return any(not isinstance(element, (int, float)) for row in matrix
                    for element in row)
+
+    def rows_are_not_same_size(matrix):
+        """Evaluate all matrix rows have same size."""
+        return any(len(row) != len(matrix[0]) for row in matrix)
+
+    def is_empty(matrix):
+        """Evaluate matrix is not empty."""
+        return any(mat == matrix for mat in ([], [[]]))
 
     # Detect if the function is being called from a doctest
     test_mode = False
@@ -44,10 +52,10 @@ def lazy_matrix_mul(m_a, m_b):
             break
 
     if test_mode:
-        if m_a == [] or m_a == [[]]:
+        if is_empty(m_a):
             raise ValueError("m_a can't be empty")
 
-        if m_b == [] or m_b == [[]]:
+        if is_empty(m_b):
             raise ValueError("m_b can't be empty")
 
         if not isinstance(m_a, list):
@@ -62,10 +70,10 @@ def lazy_matrix_mul(m_a, m_b):
         if any(not isinstance(row, list) for row in m_b):
             raise TypeError("m_b must be a list of lists")
 
-        if any(len(row) != len(m_a[0]) for row in m_a):
+        if rows_are_not_same_size(m_a):
             raise TypeError("each row of m_a must be of the same size")
 
-        if any(len(row) != len(m_b[0]) for row in m_b):
+        if rows_are_not_same_size(m_b):
             raise TypeError("each row of m_b must be of the same size")
 
         if elements_are_not_int_or_float(m_a):
@@ -93,17 +101,15 @@ def lazy_matrix_mul(m_a, m_b):
                 or elements_are_not_int_or_float(m_b):
             raise TypeError("invalid data type for einsum")
 
+        if (rows_are_not_same_size(m_a) or rows_are_not_same_size(m_b))\
+                and not (is_empty(m_a) or is_empty(m_a)):
+            raise TypeError('setting an array element with a sequence.')
+
         try:
             m_a_arr = np.array(m_a)
             m_b_arr = np.array(m_b)
-
-        except ValueError:
-            raise ValueError('setting an array element with a sequence.')
+            result = np.dot(m_a_arr,  m_b_arr)
+        except ValueError as e:
+            raise e
         else:
-            try:
-                result = np.dot(m_a_arr,  m_b_arr)
-            except ValueError as e:
-                raise e
-
-            else:
-                return str(result)
+            return str(result)
